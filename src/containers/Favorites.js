a7ddf2e664f99c8c6c2f9eff548437b8344cc7ca
clecazzz <70854934+clecazzz@users.noreply.github.com>
2021-02-21 22:50:21 +0100

    MAJ

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -3,18 +3,18 @@ import axios from "axios";
 
 const Favorites = ({ charactersId }) => {
     const [data, setData] = useState();
-    useEffect(() => {
-        const fetchData = async (event) => {
-            try {
-                const response = await axios.get(
-                    `http://localhost:3001/charactersFav/`
-                );
-            } catch (error) {
-                console.log(error.message);
-            }
-        };
-        fetchData();
-    });
+    // useEffect(() => {
+    //     const fetchData = async (event) => {
+    //         try {
+    //             const response = await axios.get(
+    //                 `http://localhost:3001/charactersFav/`
+    //             );
+    //         } catch (error) {
+    //             console.log(error.message);
+    //         }
+    //     };
+    //     fetchData();
+    // });
 
     return <div className="bg-white">{charactersId}</div>;
 };
