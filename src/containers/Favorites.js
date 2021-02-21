e882f4ec082041f57b3abed294432af66977f898
clecazzz <70854934+clecazzz@users.noreply.github.com>
2021-02-21 22:54:23 +0100

    MAJ

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 const Favorites = ({ charactersId }) => {
     const [data, setData] = useState();
